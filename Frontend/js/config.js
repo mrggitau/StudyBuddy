cb425670ee16b6825d43edb2f629b0e7ee46eee6
gitau <mark.irungu@strathmore.edu>
2026-06-29 21:59:35 +0300

    Chore: Change API_BASE for tests

--- Frontend/js/config.js
+++ Frontend/js/config.js
@@ -1,6 +1,6 @@
 // local development:
-const API_BASE = 'http://localhost:5000/api';
+//const API_BASE = 'http://localhost:5000/api';
 
-// deployed version
-// const API_BASE = 'https://studybuddy-backend-0zt7.onrender.com/api';
+// deployment
+const API_BASE = 'https://studybuddy-backend-0zt7.onrender.com/api';
 
